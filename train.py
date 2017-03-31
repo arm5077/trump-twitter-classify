e882763339f8a71733717ddb82bc5c2bb0d5d12a
Andrew McGill <arm5077@gmail.com>
2017-03-31 18:17:59 -0400

    Add custom stopwords and bump up vocab to 500

--- train.py
+++ train.py
@@ -4,27 +4,30 @@ import pickle
 import random
 from nltk.corpus import stopwords
 
+custom_stopwords = ['crookedhillary', 'kaine', 'trump2016', 'trumppence2016', 'votetrump', 'iacaucus', 'tedcruz', 'ted', 'cruz', 'bernie', 'sanders', 'trumppence16', 'jeb', 'bush', 'lyin', 'gopdebate', "lindsey", "graham"]
+
 # Read tweets and label with "trump" and "staff"
 tweets = pd.read_json("data/condensed_2016.json")
+tweets['year'] = 2016
 for year in [2017]:
-   tweets = tweets.append(pd.read_json("data/condensed_" + str(year) + ".json"))
+   temptweets = pd.read_json("data/condensed_" + str(year) + ".json")
+   temptweets['year'] = year
+   tweets = tweets.append(temptweets)
+   
 tweets['type'] = ['trump' if source =='Twitter for Android' else 'staff' for source in tweets['source']] 
 
 # Condense tweets down to simplest components
-filtered_tweets = []    
+train_tweets = [] 
+test_tweets = []
 for(index, row) in tweets.iterrows():
     if row.is_retweet == False:
-        filtered_tweets.append(( nltk.word_tokenize(row['text'].lower()), row['type']))
+        if row['year'] == 2017:
+            test_tweets.append(( nltk.word_tokenize(row['text'].lower()), row['type']))
+        else:
+            train_tweets.append(( nltk.word_tokenize(row['text'].lower()), row['type']))
 
 # Remove stopwords
-filtered_tweets = [( [ word for word in tweet[0] if word not in stopwords.words('english')], tweet[1]) for tweet in filtered_tweets]
-
-# Shuffle tweets
-random.shuffle(filtered_tweets);
-
-# Split dataset into training and regualr 
-train_tweets = filtered_tweets[(len(filtered_tweets) / 2):]
-test_tweets = filtered_tweets[:(len(filtered_tweets) / 2)]
+train_tweets = [( [ word for word in tweet[0] if word not in stopwords.words('english') and word not in custom_stopwords], tweet[1]) for tweet in train_tweets]
 
 def get_word_features(tweets):
 
@@ -38,7 +41,9 @@ def get_word_features(tweets):
     return word_features
     
 word_features = get_word_features(train_tweets)
-word_features = word_features[:100]
+word_features = word_features[:500]
+
+
 
 def extract_features(document):
     document_words = set(document)
@@ -54,10 +59,10 @@ print("beginning training of trainer")
 classifier = nltk.NaiveBayesClassifier.train(training_set)
 
 print("saving classifier")
-file = open('trump_classifier.pickle', 'wb')
+file = open('classify/trump_classifier.pickle', 'wb')
 pickle.dump(classifier, file, -1)
 file.close()
-file = open('trump_classifier_features.pickle', 'wb')
+file = open('classify/trump_classifier_features.pickle', 'wb')
 pickle.dump(word_features, file, -1)
 file.close()
 
